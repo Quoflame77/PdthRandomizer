c37f567b2e77d2197a8648c7731d37fb2487cead
Piotr Ablewski <piotr@ablewski.pl>
2025-06-12 22:21:06 +0200

    Och Grzegorz Braun, och Grzegorz Braun! Kto tak zgasić świeczkę będzie umiał!

--- src/pages/Randomizer.jsx
+++ src/pages/Randomizer.jsx
@@ -4,32 +4,40 @@ import jsonData from "../assets/data.json";
 import Item from "../components/Item";
 
 const Randomizer = () => {
-    const [data, setData] = useState(jsonData);
+  const [data, setData] = useState(jsonData);
+  const [visibleData, setVisibleData] = useState([]);
 
-    // useEffect(() => {
-    //     console.log(data);
-    //     console.log(data["handgun"]);
-    //     console.log(data["handgun"][1]);
-    //     console.log(data["handgun"][1].name);
-    // }, [])
-
-    const randomIndex = () => {
-        return Math.floor(Math.random() * 3);
-    };
-
-    return (
-        <>
-            <main className="font-orbitron h-full w-full flex flex-col">
-                <header className="w-full text-2xl text-center p-4">Randomizer</header>
-
-                <div className="h-fit w-full flex flex-col justify-center items-center gap-y-1">
-                    {Object.keys(data).map((key, index) => (
-                        <Item key={index} item={data[key][randomIndex()]} />
-                    ))}
-                </div>
-            </main>
-        </>
+  const randomize = () => {
+    const selected = Object.keys(data).map(
+      (key, index) => data[key][randomIndex()]
     );
+    setVisibleData(selected);
+  };
+
+  useEffect(() => {
+    randomize();
+  }, []);
+
+  const randomIndex = () => {
+    return Math.floor(Math.random() * 3);
+  };
+
+  return (
+    <>
+      <main className="font-orbitron h-full w-full flex flex-col">
+        <header className="w-full text-2xl text-center p-4">Randomizer</header>
+        <button type="button" onClick={randomize}>
+          XD LOL
+        </button>
+
+        <div className="h-fit w-full flex flex-col justify-center items-center gap-y-1">
+          {visibleData.map((item, index) => (
+            <Item key={index} item={item} />
+          ))}
+        </div>
+      </main>
+    </>
+  );
 };
 
 export default Randomizer;
